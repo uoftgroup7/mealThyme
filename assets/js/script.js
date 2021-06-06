81cba525970c4c5288623a36baec634ce31d0bd1
zoomzoom6 <tonyhuang107@live.ca>
2021-06-06 17:08:48 -0400

    update README and layout of recipe result

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,7 @@ var results = document.getElementById("resultBody");
 
 //search button history 
 var searchHistoryBtn = document.getElementById("searchHistoryBtn");
-var  displayCont = document.createElement("div");
+var displayCont = document.createElement("div");
 
 // get values from drop down
 var diet = document.getElementById("diet")
@@ -43,14 +43,14 @@ var incEnd = 5;
 var savRec;
 var inputTextRec;
 // Arrays
-var searchArray=[]; // saves receipe/resteraunt search
-var saveRec=[]; // saves array from fetch
+var searchArray = []; // saves receipe/resteraunt search
+var saveRec = []; // saves array from fetch
 
 //loads from local storage
 searchLS = window.localStorage.getItem('search');
 if (!searchLS) {
   searchArray = [];
-}else {
+} else {
   searchArray = JSON.parse(searchLS);
 }
 
@@ -68,7 +68,7 @@ var deactivateModal = function () {
   errorMsg2.textContent = "";
   guideMsg1.textContent = "";
   guideMsg2.textContent =
-   "";
+    "";
   cardTwo.style.display = "block";
 }
 
@@ -98,39 +98,39 @@ var searchCall = function (histBtn) {
     if (inputOne.value && inputTwo.value) {
       var inputTextRest = inputOne.value;
       var inputTextRest2 = inputTwo.value;
-      if (stateHistory===1) {
+      if (stateHistory === 1) {
         inputTextRec = histBtn;
         searchBtnClick();
         locationCheck(inputTextRest, inputTextRest2, 0);
-        stateHistory=0;
+        stateHistory = 0;
       } else {
         locationCheck(inputTextRest, inputTextRest2, 0);
-        stateHistory=0;
+        stateHistory = 0;
       }
     } else if (inputOne.value && !inputTwo.value) {
       errorMsg2.textContent = "Please enter a distance!";
     } else if (!inputOne.value && inputTwo.value) {
       errorMsg1.textContent = "Please enter a type of food!";
-    } 
-  } else if(state===1) {
+    }
+  } else if (state === 1) {
     if (inputOne.value) {
-      if(stateHistory===1) {
+      if (stateHistory === 1) {
         inputTextRec = histBtn;
         searchBtnClick();
         locationCheck(inputTextRec, 0, 1);
-        stateHistory =0; //rest value
+        stateHistory = 0; //rest value
       } else {
         inputTextRec = inputOne.value;
         searchBtnClick();
         locationCheck(inputTextRec, 0, 1);
-        stateHistory =0 //rest value
+        stateHistory = 0 //rest value
       }
     } else {
-      if(stateHistory===1) {
+      if (stateHistory === 1) {
         inputTextRec = histBtn;
         searchBtnClick();
         locationCheck(inputTextRec, 0, 1);
-        stateHistory =0; //rest value
+        stateHistory = 0; //rest value
       }
     }
   }
@@ -140,7 +140,7 @@ var searchCall = function (histBtn) {
 var locationCheck = function (input1, input2, search) {
   var apiUrl = 'https://ipfind.co/me?auth=b00ff459-9e02-42ef-be47-441a13d324b4'
 
-  fetch(apiUrl).then(function(response) {
+  fetch(apiUrl).then(function (response) {
     // if response was successful
     if (response.ok) {
       response.json().then(function (data) {
@@ -191,42 +191,44 @@ var selectSearch = function (value, dist, search, lat, lon) {
   }
 }
 
-  //list restaunts 
-  var listRestaurants = function (data) {
-    //run through and list first 10 restaurants found
-    for (var i = 0; i < 5; i++) {
-      var card = document.createElement('div');
-      card.classList = 'card';
+//list restaunts 
+var listRestaurants = function (data) {
+  //run through and list first 10 restaurants found
+  for (var i = 0; i < 5; i++) {
+    var card = document.createElement('div');
+    card.classList = 'card';
 
-      //two p tags for restaurant name and location
-      var restName = document.createElement('p');
-      var restLoc = document.createElement('p');
-      //define where to get information from
-      restName = data.hints[i].food.restaurant.address;
-      restLoc = data.hints[i].food.restaurant.postal;
-      var text1 = document.createTextNode(restName);
-      var text2 = document.createTextNode(restLoc);
+    //two p tags for restaurant name and location
+    var restName = document.createElement('p');
+    var restLoc = document.createElement('p');
+    //define where to get information from
+    restName = data.hints[i].food.restaurant.address;
+    restLoc = data.hints[i].food.restaurant.postal;
+    var text1 = document.createTextNode(restName);
+    var text2 = document.createTextNode(restLoc);
 
-      //add to card element created above
-      card.appendChild(text1);
-      card.appendChild(text2);
-      //add card to hero body
-      results.appendChild(card);
-      deactivateModal();
-    }
-
+    //add to card element created above
+    card.appendChild(text1);
+    card.appendChild(text2);
+    //add card to hero body
+    results.appendChild(card);
+    deactivateModal();
   }
 
-  var listRecipes = function (data) {
-    maxLengh = data.hits.length
-    randomIndx = Math.floor((Math.random() * (maxLengh-6)) + 1);
-    alert(maxLengh + " " + randomIndx)
-    for (var i = randomIndx; i < (randomIndx + 5); i++) {
-      var cals = Math.ceil(data.hits[i].recipe.calories);
-      var name = data.hits[i].recipe.label;
-      var imgUrl = data.hits[i].recipe.image;
-      var recUrl = data.hits[i].recipe.url;
-      results.innerHTML += `
+}
+
+var listRecipes = function (data) {
+  maxLengh = data.hits.length
+  randomIndx = Math.floor((Math.random() * (maxLengh - 6)) + 1);
+  console.log(maxLengh + " " + randomIndx);
+  for (var i = randomIndx; i < (randomIndx + 5); i++) {
+    var cals = Math.ceil(data.hits[i].recipe.calories);
+    var name = data.hits[i].recipe.label;
+    var imgUrl = data.hits[i].recipe.image;
+    console.log(imgUrl);
+    var recUrl = data.hits[i].recipe.url;
+    var sourcePage = data.hits[i].recipe.source;
+    results.innerHTML += `
     <div id="cardResult" class="card" >
       <div class="card-image">
         <figure class="image is-128x128px">
@@ -236,88 +238,87 @@ var selectSearch = function (value, dist, search, lat, lon) {
       </div>
       <div id="cardContent" class="card-content">
         <div class="content" id="` + name + `">
-          <div id="recpText"> `
-        + `${name} <br><br> ${cals} Calories` + `
-          </div>
-          <br>
-          <div>
-            <a href="` + recUrl + `" target="_blank">Click here for recipe</a>
-          </div>
+        <div>
+            <a href="` + recUrl + `" target="_blank">` + sourcePage + `</a>
+          </div>  
+        <div id="recpText"><strong> `
+      + `${name} </strong><br><br> ${cals} Calories` + `
+          </div>          
         </div>
       </div>
     </div>`
-    }
-    deactivateModal();
   }
+  deactivateModal();
+}
 
-  //function to remove all elements in the body
-  var clearScreen = function () {
-    while (results.firstChild) {
-      results.removeChild(results.firstChild);
+//function to remove all elements in the body
+var clearScreen = function () {
+  while (results.firstChild) {
+    results.removeChild(results.firstChild);
+  }
+}
+
+//added here
+function searchBtnClick() {
+  inputVal = searchStuff.value.toLowerCase().trim()
+  //searchHistoryBtn.innerHTML += `<button class="button is-primary is-rounded" id="historyButton">`+ inputVal +`</button>`
+  if (!inputVal) {
+    return
+  } else if (searchArray.includes(inputVal)) { //if already in array it will remove from array and put in top
+    indx = searchArray.indexOf(inputVal);
+    searchArray.splice(indx, 1);
+    searchArray.unshift(inputVal);
+  } else {
+    if (searchArray.length < 5) { //Only 5 buttons will populate. last one will be remove and new entry will be in top
+      searchArray.unshift(inputVal);
+    } else {
+      searchArray.pop();
+      searchArray.unshift(inputVal);
     }
   }
+  window.localStorage.setItem("search", JSON.stringify(searchArray));
+  // check to see if city inputed already, if blank nothing happens
+}
 
-  //added here
-  function searchBtnClick () {
-    inputVal = searchStuff.value.toLowerCase().trim()
-    //searchHistoryBtn.innerHTML += `<button class="button is-primary is-rounded" id="historyButton">`+ inputVal +`</button>`
-    if (!inputVal) {
-      return
-    } else if (searchArray.includes(inputVal)) { //if already in array it will remove from array and put in top
-        indx = searchArray.indexOf(inputVal);
-        searchArray.splice(indx, 1);
-        searchArray.unshift(inputVal);
-     } else {
-         if(searchArray.length < 5){ //Only 5 buttons will populate. last one will be remove and new entry will be in top
-          searchArray.unshift(inputVal);
-         } else {
-          searchArray.pop();
-          searchArray.unshift(inputVal);
-         }
-       }
-       window.localStorage.setItem("search",JSON.stringify(searchArray));
-    // check to see if city inputed already, if blank nothing happens
-  }
-  
-  function goNext (){
-    incStart = 5;
-    incEnd = 10;
-    listRecipes(savRec,incStart,incEnd,1)
-    incStart = 0;
-    incEnd = 5;
-  }
-  //done
-  
-  //show history button
-  function showHistoryBtn (){
-    //clear previous
-    searchHistoryBtn.innerHTML="";
-    histLen = searchArray.length;
-    for (i=0;i<histLen;i++){
-      searchHistoryBtn.innerHTML += 
+function goNext() {
+  incStart = 5;
+  incEnd = 10;
+  listRecipes(savRec, incStart, incEnd, 1)
+  incStart = 0;
+  incEnd = 5;
+}
+//done
+
+//show history button
+function showHistoryBtn() {
+  //clear previous
+  searchHistoryBtn.innerHTML = "";
+  histLen = searchArray.length;
+  for (i = 0; i < histLen; i++) {
+    searchHistoryBtn.innerHTML +=
       `<button class="button is-primary is-rounded is-small" id="historyButton">`
-      +searchArray[i]
-      +`</button>`
-    }
+      + searchArray[i]
+      + `</button>`
   }
+}
 
-  //store value for history button
-  function storeVal () {
-   
-  };
+//store value for history button
+function storeVal() {
+
+};
 
 
-  restBtn.addEventListener("click", activateModal);
-  restBtn.addEventListener("click", restaurantModal);
-  recipeBtn.addEventListener("click", activateModal);
-  recipeBtn.addEventListener("click", recipeModal);
-  modalClose1.addEventListener("click", deactivateModal);
-  modalClose2.addEventListener("click", deactivateModal);
-  searchBtn.addEventListener("click", searchCall);
-  //moreBtn.addEventListener("click");
-  document.body.addEventListener( 'click', function ( event ) {
-  if( event.target.id == 'historyButton' ) {
-    stateHistory=1;
+restBtn.addEventListener("click", activateModal);
+restBtn.addEventListener("click", restaurantModal);
+recipeBtn.addEventListener("click", activateModal);
+recipeBtn.addEventListener("click", recipeModal);
+modalClose1.addEventListener("click", deactivateModal);
+modalClose2.addEventListener("click", deactivateModal);
+searchBtn.addEventListener("click", searchCall);
+//moreBtn.addEventListener("click");
+document.body.addEventListener('click', function (event) {
+  if (event.target.id == 'historyButton') {
+    stateHistory = 1;
     searchCall(event.target.innerHTML);
   };
 });
